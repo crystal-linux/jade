a70c76305087795b4799b5f3f21ba09444f742af
Amy <axtlos@salyut.one>
2022-01-04 23:46:22 +0100

    setup basic echo websocket server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+mod mods;
+use std::{collections::HashMap, convert::Infallible, sync::Arc, fs, fs::File, io::prelude::*, env};
+use tokio::sync::{mpsc, Mutex};
+use warp::{ws::Message, Filter, Rejection};
+use toml;
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+struct General {
+    websocket_ip: String,
+    websocket_port: String,
+    restore_conf: String,
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Client {
+    pub client_id: String,
+    pub sender: Option<mpsc::UnboundedSender<std::result::Result<Message, warp::Error>>>,
+}
+
+type Clients = Arc<Mutex<HashMap<String, Client>>>;
+type Result<T> = std::result::Result<T, Rejection>;
+
+#[tokio::main]
+async fn main() {
+    let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
+    println!("Configuring websocket route");
+    let ws_route = warp::path("ws")
+        .and(warp::ws())
+        .and(with_clients(clients.clone()))
+        .and_then(mods::handlers::ws_handler);
+
+    let routes = ws_route.with(warp::cors().allow_any_origin());
+    println!("Starting server");
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
+}
+
+fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
+    warp::any().map(move || clients.clone())
 }
