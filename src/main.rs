40c95eaa5cb40744d45bfec1cd01fe1e568b39a9
amy <axtlos@tar.black>
2022-01-19 19:07:55 +0100

    nuke everything and start a new cargo project

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-mod mods;
-use std::{collections::HashMap, convert::Infallible, sync::Arc, env};
-use tokio::sync::{mpsc, Mutex};
-use warp::{ws::Message, Filter, Rejection};
-
-#[derive(Debug, Clone)]
-pub struct Client {
-    pub client_id: String,
-    pub sender: Option<mpsc::UnboundedSender<std::result::Result<Message, warp::Error>>>,
-}
-
-type Clients = Arc<Mutex<HashMap<String, Client>>>;
-type Result<T> = std::result::Result<T, Rejection>;
-
-#[tokio::main]
-async fn main() {
-    let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
-    println!("Configuring websocket route");
-    let ws_route = warp::path("ws")
-        .and(warp::ws())
-        .and(with_clients(clients.clone()))
-        .and_then(mods::handlers::ws_handler);
-
-    let routes = ws_route.with(warp::cors().allow_any_origin());
-    println!("Starting server");
-    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
-}
-
-fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
-    warp::any().map(move || clients.clone())
+fn main() {
+    println!("Hello, world!");
 }
