caff22cb3e3189a6deec26b5faf1357e3ea0933b
amy <axtlos@tar.black>
2022-01-23 15:46:56 +0100

    make it actually output the install status and pass the returncode to exec_eval

--- src/internal/install.rs
+++ src/internal/install.rs
@@ -1,9 +1,9 @@
 use std::process::Command;
+use crate::internal::*;
 
 pub fn install(pkgs: Vec<&str>) {
-    Command::new("crystalstrap")
+    exec_eval(Command::new("crystalstrap")
         .arg("/mnt")
-        .args(pkgs)
-        .output()
-        .expect("Failed to install packages");
+        .args(&pkgs)
+        .status(), format!("Install packages {}", pkgs.join(", ")).as_str());
 }
