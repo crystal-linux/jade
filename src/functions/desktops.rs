5eafc9de68e6892391ce750fac428c13c9dfd65e
amy <axtlos@tar.black>
2022-04-18 17:02:07 +0000

    Update desktops.rs

--- src/functions/desktops.rs
+++ src/functions/desktops.rs
@@ -3,7 +3,7 @@ use crate::internal::*;
 
 pub fn choose_pkgs(desktop_setup: &str) {
     log(format!("Installing {}", desktop_setup));
-    match desktop_setup {
+    match desktop_setup.lowercase() {
         "onyx" => {
             install(vec![
                 "xorg",
