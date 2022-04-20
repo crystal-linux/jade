152551ec927e46c10b7d6682163b9403570ec01f
trivernis <trivernis@protonmail.com>
2022-04-20 12:05:48 +0200

    Change argument parsing to use clap v3 derive
    Signed-off-by: trivernis <trivernis@protonmail.com>

--- src/functions/desktops.rs
+++ src/functions/desktops.rs
@@ -1,143 +1,23 @@
+use crate::args::DesktopSetup;
 use crate::internal::exec::*;
 use crate::internal::*;
 
-pub fn choose_pkgs(desktop_setup: &str) {
-    log(format!("Installing {}", desktop_setup));
-    match desktop_setup.to_lowercase().as_str() {
-        "onyx" => {
-            install(vec![
-                "xorg",
-                "onyx",
-                "lightdm",
-                "lightdm-gtk-greeter",
-                "lightdm-gtk-greeter-settings",
-            ]);
-            files_eval(
-                files::append_file(
-                    "/mnt/etc/lightdm/lightdm.conf",
-                    "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
-                ),
-                "Add lightdm greeter",
-            );
-            enable_dm("lightdm");
-        }
-        "gnome" => {
-            install(vec![
-                "xorg",
-                "gnome",
-                "gnome-tweaks",
-                "chrome-gnome-shell",
-                "gdm",
-            ]);
-            enable_dm("gdm");
-        }
-        "kde" => {
-            install(vec![
-                "xorg",
-                "plasma",
-                "plasma-wayland-session",
-                "kde-utilities",
-                "kde-system",
-                "sddm",
-            ]);
-            enable_dm("sddm");
-        }
-        "budgie" => {
-            install(vec![
-                "xorg",
-                "budgie-desktop",
-                "gnome",
-                "lightdm",
-                "lightdm-gtk-greeter",
-                "lightdm-gtk-greeter-settings",
-            ]);
-            files_eval(
-                files::append_file(
-                    "/mnt/etc/lightdm/lightdm.conf",
-                    "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
-                ),
-                "Add lightdm greeter",
-            );
-            enable_dm("lightdm");
-        }
-        "cinnamon" => {
-            install(vec![
-                "xorg",
-                "cinnamon",
-                "lightdm",
-                "lightdm-gtk-greeter",
-                "lightdm-gtk-greeter-settings",
-                "metacity",
-                "gnome-shell",
-            ]);
-            files_eval(
-                files::append_file(
-                    "/mnt/etc/lightdm/lightdm.conf",
-                    "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
-                ),
-                "Add lightdm greeter",
-            );
-            enable_dm("lightdm");
-        }
-        "mate" => {
-            install(vec![
-                "xorg",
-                "mate",
-                "lightdm",
-                "lightdm-gtk-greeter",
-                "lightdm-gtk-greeter-settings",
-                "mate-extra",
-            ]);
-            files_eval(
-                files::append_file(
-                    "/mnt/etc/lightdm/lightdm.conf",
-                    "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
-                ),
-                "Add lightdm greeter",
-            );
-            enable_dm("lightdm");
-        }
-        "xfce" => {
-            install(vec![
-                "xorg",
-                "xfce4",
-                "lightdm",
-                "lightdm-gtk-greeter",
-                "lightdm-gtk-greeter-settings",
-                "xfce4-goodies",
-            ]);
-            files_eval(
-                files::append_file(
-                    "/mnt/etc/lightdm/lightdm.conf",
-                    "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
-                ),
-                "Add lightdm greeter",
-            );
-            enable_dm("lightdm");
-        }
-        "enlightenment" => {
-            install(vec![
-                "xorg",
-                "enlightenment",
-                "lightdm",
-                "lightdm-gtk-greeter",
-                "lightdm-gtk-greeter-settings",
-                "terminology",
-            ]);
-            files_eval(
-                files::append_file(
-                    "/mnt/etc/lightdm/lightdm.conf",
-                    "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
-                ),
-                "Add lightdm greeter",
-            );
-            enable_dm("lightdm");
-        }
-
-        _ => {
-            crash("Unknown desktop setup".to_string(), 1);
-        }
+pub fn install_desktop_setup(desktop_setup: DesktopSetup) {
+    log(format!("Installing {:?}", desktop_setup));
+    match desktop_setup {
+        DesktopSetup::Onyx => install_onyx(),
+        DesktopSetup::Gnome => install_gnome(),
+        DesktopSetup::Kde => install_kde(),
+        DesktopSetup::Budgie => install_budgie(),
+        DesktopSetup::Cinnamon => install_cinnamon(),
+        DesktopSetup::Mate => install_mate(),
+        DesktopSetup::Xfce => install_xfce(),
+        DesktopSetup::Enlightenment => install_enlightenment(),
     }
+    install_networkmanager();
+}
+
+fn install_networkmanager() {
     install(vec!["networkmanager"]);
     exec_eval(
         exec_chroot(
@@ -146,6 +26,143 @@ pub fn choose_pkgs(desktop_setup: &str) {
         ),
         "Enable network manager",
     );
+}
+
+fn install_enlightenment() {
+    install(vec![
+        "xorg",
+        "enlightenment",
+        "lightdm",
+        "lightdm-gtk-greeter",
+        "lightdm-gtk-greeter-settings",
+        "terminology",
+    ]);
+    files_eval(
+        files::append_file(
+            "/mnt/etc/lightdm/lightdm.conf",
+            "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
+        ),
+        "Add lightdm greeter",
+    );
+    enable_dm("lightdm");
+}
+
+fn install_xfce() {
+    install(vec![
+        "xorg",
+        "xfce4",
+        "lightdm",
+        "lightdm-gtk-greeter",
+        "lightdm-gtk-greeter-settings",
+        "xfce4-goodies",
+    ]);
+    files_eval(
+        files::append_file(
+            "/mnt/etc/lightdm/lightdm.conf",
+            "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
+        ),
+        "Add lightdm greeter",
+    );
+    enable_dm("lightdm");
+}
+
+fn install_mate() {
+    install(vec![
+        "xorg",
+        "mate",
+        "lightdm",
+        "lightdm-gtk-greeter",
+        "lightdm-gtk-greeter-settings",
+        "mate-extra",
+    ]);
+    files_eval(
+        files::append_file(
+            "/mnt/etc/lightdm/lightdm.conf",
+            "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
+        ),
+        "Add lightdm greeter",
+    );
+    enable_dm("lightdm");
+}
+
+fn install_cinnamon() {
+    install(vec![
+        "xorg",
+        "cinnamon",
+        "lightdm",
+        "lightdm-gtk-greeter",
+        "lightdm-gtk-greeter-settings",
+        "metacity",
+        "gnome-shell",
+    ]);
+    files_eval(
+        files::append_file(
+            "/mnt/etc/lightdm/lightdm.conf",
+            "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
+        ),
+        "Add lightdm greeter",
+    );
+    enable_dm("lightdm");
+}
+
+fn install_budgie() {
+    install(vec![
+        "xorg",
+        "budgie-desktop",
+        "gnome",
+        "lightdm",
+        "lightdm-gtk-greeter",
+        "lightdm-gtk-greeter-settings",
+    ]);
+    files_eval(
+        files::append_file(
+            "/mnt/etc/lightdm/lightdm.conf",
+            "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
+        ),
+        "Add lightdm greeter",
+    );
+    enable_dm("lightdm");
+}
+
+fn install_kde() {
+    install(vec![
+        "xorg",
+        "plasma",
+        "plasma-wayland-session",
+        "kde-utilities",
+        "kde-system",
+        "sddm",
+    ]);
+    enable_dm("sddm");
+}
+
+fn install_gnome() {
+    install(vec![
+        "xorg",
+        "gnome",
+        "gnome-tweaks",
+        "chrome-gnome-shell",
+        "gdm",
+    ]);
+    enable_dm("gdm");
+}
+
+fn install_onyx() {
+    install(vec![
+        "xorg",
+        "onyx",
+        "lightdm",
+        "lightdm-gtk-greeter",
+        "lightdm-gtk-greeter-settings",
+    ]);
+    files_eval(
+        files::append_file(
+            "/mnt/etc/lightdm/lightdm.conf",
+            "[SeatDefaults]\ngreeter-session=lightdm-gtk-greeter\n",
+        ),
+        "Add lightdm greeter",
+    );
+    enable_dm("lightdm");
 }
 
 fn enable_dm(dm: &str) {
--- src/main.rs
+++ src/main.rs
@@ -1,213 +1,62 @@
+mod args;
 mod functions;
 mod internal;
 
+use crate::args::{BootloaderSubcommand, Command, Opt, UsersSubcommand};
 use crate::functions::*;
-use clap::{App, Arg, SubCommand};
+use clap::Parser;
 
 fn main() {
-    let app = App::new("jade")
-        .version(env!("CARGO_PKG_VERSION"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .subcommand(
-            SubCommand::with_name("partition")
-                .about("Partition the install destination")
-                .arg(
-                    Arg::with_name("mode")
-                        .help("If jade should automatically partition (mode = auto) or the user manually partitioned it (mode = manual)")
-                        .possible_values(&["auto", "manual"])
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("device")
-                        .help("The device to partition")
-                        .required_if("mode", "auto"),
-                )
-                .arg(
-                    Arg::with_name("efi")
-                        .help("If the install destination should be partitioned with EFI")
-                        .long("efi")
-                        .takes_value(false),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("install-base")
-                .about("Install base packages")
-        )
-        .subcommand(
-            SubCommand::with_name("genfstab")
-                .about("Generate fstab")
-        )
-        .subcommand(
-            SubCommand::with_name("setup-timeshift")
-                .about("Setup timeshift")
-        )
-        .subcommand(
-            SubCommand::with_name("bootloader")
-                .about("Install bootloader")
-                .subcommand(
-                    SubCommand::with_name("grub-efi")
-                        .about("Install grub-efi")
-                        .arg(
-                            Arg::with_name("efidir")
-                                .help("The directory to install the EFI bootloader to")
-                                .required(true),
-                        ),
-                )
-                .subcommand(
-                    SubCommand::with_name("grub-legacy")
-                        .about("Install grub-legacy")
-                        .arg(
-                            Arg::with_name("device")
-                                .help("The device to install the bootloader to")
-                                .required(true),
-                        ),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("locale")
-                .about("Set locale stuff")
-                .arg(
-                    Arg::with_name("keyboard")
-                        .help("The keyboard layout to use")
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("timezone")
-                        .help("The timezone to use")
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("locales")
-                        .help("The locales to set")
-                        .multiple(true)
-                        .index(3)
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("networking")
-                .about("Set networking stuff")
-                .arg(
-                    Arg::with_name("hostname")
-                        .help("The hostname to use")
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("ipv6")
-                        .help("Wether ipv6 should be enabled")
-                        .short("i6")
-                        .long("ipv6")
-                        .takes_value(false),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("users")
-                .about("Configure users")
-                .subcommand(
-                    SubCommand::with_name("newUser")
-                        .about("Create a new user")
-                        .arg(
-                            Arg::with_name("username")
-                                .help("The username to create")
-                                .required(true),
-                        )
-                        .arg(
-                            Arg::with_name("hasroot")
-                                .help("If the user should have root privileges")
-                                .required(true),
-                        )
-                        .arg(
-                            Arg::with_name("password")
-                                .help("The password to set. NOTE: Takes hashed password, use `openssl passwd -1 <password>` to generate the hash.")
-                                .required(true),
-                        )
-                    )
-                .subcommand(
-                    SubCommand::with_name("rootPass")
-                        .about("Set the root password")
-                        .arg(
-                            Arg::with_name("rootPass")
-                                .help("The password to set. NOTE: Takes hashed password, use `openssl passwd -1 <password>` to generate the hash._")
-                                .required(true),
-                        ),
-                )
-        )
-        .subcommand(
-            SubCommand::with_name("nix")
-                .about("Install nix"),
-        )
-        .subcommand(
-            SubCommand::with_name("config")
-                .about("read a jade installation config")
-                .arg(
-                    Arg::with_name("config")
-                        .help("The config to read")
-                        .required(true),
-                ),
-            )
-        .subcommand(
-            SubCommand::with_name("desktops")
-                .about("Graphical stuff (Desktop environment and Display Manager)")
-                .arg(
-                    Arg::with_name("desktopsetup")
-                        .help("The desktop setup to use")
-                        .required(true),
-                ),
-    ).get_matches();
-
-    if let Some(app) = app.subcommand_matches("partition") {
-        partition::partition(
-            app.value_of("device").unwrap(),
-            app.value_of("mode").unwrap(),
-            app.is_present("efi"),
-        );
-    } else if let Some(app) = app.subcommand_matches("locale") {
-        locale::set_locale(
-            app.values_of("locales")
-                .unwrap()
-                .collect::<Vec<&str>>()
-                .join(" "),
-        );
-        locale::set_keyboard(app.value_of("keyboard").unwrap());
-        locale::set_timezone(app.value_of("timezone").unwrap());
-    } else if let Some(app) = app.subcommand_matches("networking") {
-        if app.is_present("ipv6") {
-            network::create_hosts();
-            network::enable_ipv6()
-        } else {
-            network::create_hosts()
+    let opt: Opt = Opt::parse();
+    match opt.command {
+        Command::Partition(args) => {
+            partition::partition(args.device, args.mode, args.efi);
         }
-        network::set_hostname(app.value_of("hostname").unwrap())
-    } else if let Some(app) = app.subcommand_matches("users") {
-        if let Some(app) = app.subcommand_matches("newUser") {
-            users::new_user(
-                app.value_of("username").unwrap(),
-                app.value_of("hasroot").unwrap().parse::<bool>().unwrap(),
-                app.value_of("password").unwrap(),
-            );
-        } else if let Some(app) = app.subcommand_matches("rootPass") {
-            users::root_pass(app.value_of("rootPass").unwrap());
+        Command::InstallBase => {
+            base::install_base_packages();
         }
-    } else if let Some(app) = app.subcommand_matches("desktops") {
-        desktops::choose_pkgs(app.value_of("desktopsetup").unwrap());
-    } else if let Some(app) = app.subcommand_matches("bootloader") {
-        if let Some(app) = app.subcommand_matches("grub-efi") {
-            base::install_bootloader_efi(app.value_of("efidir").unwrap());
-        } else if let Some(app) = app.subcommand_matches("grub-legacy") {
-            base::install_bootloader_legacy(app.value_of("device").unwrap());
+        Command::GenFstab => {
+            base::genfstab();
         }
-    } else if app.subcommand_matches("install-base").is_some() {
-        base::install_base_packages();
-    } else if app.subcommand_matches("genfstab").is_some() {
-        base::genfstab();
-    } else if app.subcommand_matches("setup-timeshift").is_some() {
-        base::setup_timeshift();
-    } else if app.subcommand_matches("nix").is_some() {
-        base::install_homemgr();
-    } else if let Some(app) = app.subcommand_matches("config") {
-        crate::internal::config::read_config(app.value_of("config").unwrap());
-    } else {
-        println!("{}", app.usage());
+        Command::SetupTimeshift => base::setup_timeshift(),
+        Command::Bootloader { subcommand } => match subcommand {
+            BootloaderSubcommand::GrubEfi { efidir } => {
+                base::install_bootloader_efi(efidir);
+            }
+            BootloaderSubcommand::GrubLegacy { device } => {
+                base::install_bootloader_legacy(device);
+            }
+        },
+        Command::Locale(args) => {
+            locale::set_locale(args.locales.join(" "));
+            locale::set_keyboard(&args.keyboard);
+            locale::set_timezone(&args.timezone);
+        }
+        Command::Networking(args) => {
+            if args.ipv6 {
+                network::create_hosts();
+                network::enable_ipv6()
+            } else {
+                network::create_hosts();
+            }
+            network::set_hostname(&args.hostname);
+        }
+        Command::Users { subcommand } => match subcommand {
+            UsersSubcommand::NewUser(args) => {
+                users::new_user(&args.username, args.hasroot, &args.password);
+            }
+            UsersSubcommand::RootPass { password } => {
+                users::root_pass(&password);
+            }
+        },
+        Command::Nix => {
+            base::install_homemgr();
+        }
+        Command::Config { config } => {
+            crate::internal::config::read_config(config);
+        }
+        Command::Desktops { desktop } => {
+            desktops::install_desktop_setup(desktop);
+        }
     }
 }
